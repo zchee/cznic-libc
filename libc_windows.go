8af8d7cd897ec0646400924a4b29c5cf5021d2a5
Jan Mercl <0xjnml@gmail.com>
2020-10-09 16:54:13 +0200

    WIPS

--- libc_windows.go
+++ libc_windows.go
@@ -144,7 +144,7 @@ var (
 
 func init() {
 	mustLinkDll("kernel32.dll", []linkFunc{
-		{"PeekConsoleInput", &peekConsoleInputW},
+		{"PeekConsoleInputW", &peekConsoleInputW},
 		{"ResetEvent", &resetEvent},
 		{"WriteConsoleW", &writeConsoleW},
 		{"ReadConsoleW", &readConsoleW},
