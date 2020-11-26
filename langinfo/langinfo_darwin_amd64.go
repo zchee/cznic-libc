ce2cbd00a0a4da385610f2dab1655b8c1d969659
Jan Mercl <0xjnml@gmail.com>
2020-11-26 16:12:30 +0100

    darwin: generate

--- langinfo/langinfo_darwin_amd64.go
+++ langinfo/langinfo_darwin_amd64.go
@@ -98,8 +98,8 @@ type Wchar_t = int32 /* <builtin>:15:24 */
 type X__int128_t = [2]int64   /* <builtin>:21:24 */ //TODO
 type X__uint128_t = [2]uint64 /* <builtin>:22:25 */ //TODO
 
-type X__builtin_va_list = uintptr /* <builtin>:34:14 */
-type X__float128 = float64        /* <builtin>:35:21 */
+type X__builtin_va_list = uintptr /* <builtin>:38:14 */
+type X__float128 = float64        /* <builtin>:39:21 */
 
 // -
 // Copyright (c) 2001 Alexey Zelkin <phantom@FreeBSD.org>
